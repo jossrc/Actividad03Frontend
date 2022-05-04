950e40a8e89ba49c5ac1b369c0f26e88870d49b3
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-05-04 16:58:06 -0500

    ajustes para la consulta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConsultaDocenteComponent } from './components/consulta-docente/consulta-docente.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultaDocenteComponent
   ],
   imports: [
     BrowserModule,
